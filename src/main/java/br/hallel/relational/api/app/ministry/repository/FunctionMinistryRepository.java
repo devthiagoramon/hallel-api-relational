bdcc7bc77f3b269b562183172ff703a421c379b1
devthiagoramon <dev.thiagoramon@gmail.com>
2025-06-03 08:22:06 -0400

    fix - listing function Ministry repository fix

--- src/main/java/br/hallel/relational/api/app/ministry/repository/FunctionMinistryRepository.java
+++ src/main/java/br/hallel/relational/api/app/ministry/repository/FunctionMinistryRepository.java
@@ -10,20 +10,23 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface FunctionMinistryRepository extends JpaRepository<FunctionMinistry, UUID> {
+public interface FunctionMinistryRepository
+        extends JpaRepository<FunctionMinistry, UUID> {
 
-    @Query(
-            value = """
-                    select * from function_ministry fm where fm.ministry_id = :ministry_id
-                    """, nativeQuery = true
+    @Query("""
+            select new br.hallel.relational.api.app.ministry.dto.FunctionMinistryResponse(fm.id, fm.ministryId, fm.name, fm.description, fm.color, fm.icon)
+            from function_ministry fm
+            where fm.ministryId = :ministry_id
+            """
     )
-    List<FunctionMinistryResponse> listAllFunctionsByMinistryId(@Param("ministry_id") UUID ministryId);
+    List<FunctionMinistryResponse> listAllFunctionsByMinistryId(
+            @Param("ministry_id") UUID ministryId);
 
-    @Query(
-            value = """
-                    select * from function_ministry fm where fm.id = :id
-                    """, nativeQuery = true
+    @Query("""
+            select fm from function_ministry fm where fm.id = :id
+            """
     )
-    Optional<FunctionMinistry> listById(@Param("id") UUID functionMinistryId);
+    Optional<FunctionMinistry> listById(
+            @Param("id") UUID functionMinistryId);
 
 }
