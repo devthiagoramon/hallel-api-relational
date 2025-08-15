da87282e97464799198febcb1b69f12fa4273243
MIGUEL-ARCANJO-BRASIL <devmiguelarcanjo@gmail.com>
2025-08-15 18:35:00 -0400

    fix again gcpconfig

--- src/main/java/br/hallel/relational/api/app/global/config/GcpConfig.java
+++ src/main/java/br/hallel/relational/api/app/global/config/GcpConfig.java
@@ -3,49 +3,13 @@ package br.hallel.relational.api.app.global.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class GcpConfig {
-
-    private static final String TEMP_CREDENTIALS_PATH = System.getProperty("java.io.tmpdir") + "/gcp-key.json";
-
-    @PostConstruct
-    public void init() throws Exception {
-        InputStream inputStream = getClass().getClassLoader()
-                .getResourceAsStream("crypto-avatar.json");
-        if (inputStream == null) {
-            throw new RuntimeException("Arquivo de credenciais não encontrado no resources!");
-        }
-        Path tempPath = Paths.get(TEMP_CREDENTIALS_PATH).getParent();
-        if (tempPath != null) {
-            Files.createDirectories(tempPath);
-        }
-
-        File tempFile = new File(TEMP_CREDENTIALS_PATH);
-        try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length);
-            }
-
-            System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", TEMP_CREDENTIALS_PATH);
-
-        }
-
-
-    }
 
     @Bean
     public Storage storage() throws IOException {
@@ -54,5 +18,4 @@ public class GcpConfig {
                 .build()
                 .getService();
     }
-
 }
