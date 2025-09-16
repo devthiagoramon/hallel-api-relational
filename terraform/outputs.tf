f6c10b5b745e30ff64ca8ecd9e2ab6deeec4ddb6
devthiagoramon <dev.thiagoramon@gmail.com>
2025-09-16 19:04:32 -0400

    prod - deploy.yml fixes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,3 +18,9 @@ output "app_security_group_id" {
   description = "O ID do Security Group da aplicação, para ser usado no deploy do EC2/Beanstalk."
   value = aws_security_group.app_sg.id
 }
+
+output "db_password" {
+  description = "A senha gerada para o banco de dados RDS."
+  value       = random_password.db_password.result
+  sensitive   = true # Impede que a senha apareça nos logs do Terraform
+}
